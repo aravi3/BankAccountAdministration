2485ee67bac3a99af984b9ca22c6918782eab0e2
Arvind Ravi <ar2au@virginia.edu>
2018-06-29 22:53:05 -0400

    Create list of objects

--- BankAccountApp.java
+++ BankAccountApp.java
@@ -1,17 +1,12 @@
+import java.util.LinkedList;
 import java.util.List;
 
 public class BankAccountApp {
     public static void main(String[] args) {
-        String file = "/Users/arvindravi/Desktop/Java Projects/BankAccountAdministration/NewBankAccounts.csv";
-
-        // Checking chkacc1 = new Checking("Tom Wilson","321456879",1500);
-        // Savings savacc1 = new Savings("Rich Lowe","456657897",2500);
-        
-        // savacc1.showInfo();
-        // System.out.println("***********");
-        // chkacc1.showInfo();
+        List<Account> accounts = new LinkedList<Account>();
 
         // Read a CSV file then create new accounts based on that data
+        String file = "/Users/arvindravi/Desktop/Java Projects/BankAccountAdministration/NewBankAccounts.csv";
         CSV file1 = new CSV();
         List<String[]> newAccountHolders = file1.read(file);
         for (String[] accountHolder : newAccountHolders) {
@@ -19,14 +14,18 @@ public class BankAccountApp {
             String SSN = accountHolder[1];
             String accountType = accountHolder[2];
             double initDeposit = Double.parseDouble(accountHolder[3]);
-            // System.out.println(name + " " + SSN + " " + accountType + " $" + initDeposit);
             if (accountType.equals("Savings")) {
-                System.out.println("OPEN A SAVINGS ACCOUNT");
+                accounts.add(new Savings(name, SSN, initDeposit));
             } else if (accountType.equals("Checking")) {
-                System.out.println("OPEN A CHECKING ACCOUNT");
+                accounts.add(new Checking(name, SSN, initDeposit));
             } else {
                 System.out.println("ERROR READING ACCOUNT TYPE");
             }
+        }
+
+        for (Account acc : accounts) {
+            System.out.println("\n*****************");
+            acc.showInfo();
         }
     }
 }
